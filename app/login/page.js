2f7f12c7741e45b32903a048dab3bd73187a2971
Alena Roshchyna <girls.code.better@gmail.com>
2023-06-19 18:19:12 +0200

    Create database, add api and auth

--- app/login/page.js
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <main>
-      <h1>About FoxySleep</h1>
-    </main>
-  );
-}
--- app/register/page.js
+++
@@ -1,7 +0,0 @@
-export default function RegisterPage() {
-  return (
-    <main>
-      <h1>About FoxySleep</h1>
-    </main>
-  );
-}
